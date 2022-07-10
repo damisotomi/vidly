2f639f58f765c43f096eceb44d7310cce7fcbb13
damisotomi <sotomidamilola@gmail.com>
2022-07-10 22:00:59 +0100

    Adding tk-tools to the requirements list

---
+++ venv/Lib/site-packages/tk_tools/__init__.py
@@ -0,0 +1,51 @@
+from tk_tools.canvas import Gauge, Graph, Led, RotaryScale
+
+from tk_tools.groups import (
+    ButtonGrid,
+    Calendar,
+    EntryGrid,
+    KeyValueEntry,
+    LabelGrid,
+    MultiSlotFrame,
+    SevenSegment,
+    SevenSegmentDigits,
+)
+
+from tk_tools.widgets import (
+    BinaryLabel,
+    ByteLabel,
+    ScrollableFrame,
+    SmartCheckbutton,
+    SmartListBox,
+    SmartOptionMenu,
+    SmartSpinBox,
+)
+
+from tk_tools.tooltips import ToolTip
+
+from tk_tools.version import __version__
+
+
+__all__ = [
+    "BinaryLabel",
+    "ButtonGrid",
+    "Calendar",
+    "ByteLabel",
+    "EntryGrid",
+    "Gauge",
+    "Graph",
+    "KeyValueEntry",
+    "LabelGrid",
+    "Led",
+    "MultiSlotFrame",
+    "RotaryScale",
+    "ScrollableFrame",
+    "SmartCheckbutton",
+    "SmartListBox",
+    "SmartOptionMenu",
+    "SmartSpinBox",
+    "SevenSegment",
+    "SevenSegmentDigits",
+    "ToolTip",
+    "__version__",
+]
